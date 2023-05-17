cf656f797246297ca533ae5ada22069eddb87668
Rhallen Carlos <rhallenc@gmail.com>
2023-05-17 11:55:26 -0300

    Update

--- introducao/js/app.js
+++ introducao/js/app.js
@@ -1,10 +1,20 @@
+let msg ='Bem vindo!';
+let original= 'Texto Original!';
+let substituto = 'Texto Substituto';
+
+window.alert(msg);
+
 function trocaTexto(){
 
-  document.getElementById('troca').innerHTML = 'Texto Substituto';
+  document.getElementById('troca').innerHTML = substituto;
 
 }
 function voltaTexto(){
 
-  document.getElementById('troca').innerHTML = 'Texto Original';
+  document.getElementById('troca').innerHTML = original;
 
+}
+
+function printar(){
+  window.print();
 }
